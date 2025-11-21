70a9807dbce62754e0b25b63939b4f0e74cae148
Fre-Ar <armando.freitas.001@student.uni.lu>
2025-11-21 21:29:10 +0100

    removed useless imports in hashkey/users

--- src/app/[hashkey]/users/page.tsx
+++ src/app/[hashkey]/users/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useRef} from 'react';
-import { useQuiz, Group, GroupSettings, User } from '@/components/session-context';
-import { useRouter } from 'next/navigation';
+import React, { useState} from 'react';
 
 import Header from '@/components/header';
 import NavMenu from '@/components/nav-menu';
