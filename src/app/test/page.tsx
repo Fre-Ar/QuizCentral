133cf3e54bb1b0df93d6503ead6b59eec7075788
Fre-Ar <armando.freitas.001@student.uni.lu>
2025-12-04 21:22:01 +0100

    updated packages

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -10,7 +10,7 @@ export default function TestPage() {
     <QuizProvider schema={MOCK_SCHEMA}>
       <div className="min-h-screen flex flex-col">
         <Header />
-        <main className="flex-grow flex justify-center">
+        <main className="grow flex justify-center">
           <div className="flex flex-col min-w-full border-l border-r border-uni-grey mx-4 lg:mx-0">
                <QuizRenderer />
           </div>
