78e26c690b3a829204c5905f7fc804aaa8775432
Kylian Kinnen <0231733556@uni.lu>
2025-12-08 16:13:52 +0100

    added response parsing and transforming into blocks to openAI call

--- src/hooks/ai.ts
+++ src/hooks/ai.ts
@@ -122,8 +122,14 @@ export function useOpenAPISending(apiKey: string | null){
         });
 
         const text = completion.choices[0]?.message?.content ?? '';
+        const responseJson = JSON.parse(text);
+        const blocks =(responseJson as Array<TemplateInstance>)
+
+        
         console.log('OpenAI response:', text);
+        
         setReply(text);
+        return blocks;
         } catch (err) {
         console.error(err);
         setReply('Error calling OpenAI. Check console and your API key.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
